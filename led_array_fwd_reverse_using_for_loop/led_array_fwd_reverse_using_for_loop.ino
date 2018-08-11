9feef201edce2e1fa8353b1330c49100f06ea23b
Rajesh Krishna <38561827+mvrk33@users.noreply.github.com>
2018-08-11 07:34:49 +0530

    Day-2 (Task)
    Task

--- led_array_fwd_reverse_using_for_loop/led_array_fwd_reverse_using_for_loop.ino
+++ led_array_fwd_reverse_using_for_loop/led_array_fwd_reverse_using_for_loop.ino
@@ -1,19 +1,19 @@
 void setup() {
 for(int i=8;i<=13;i++)
 {
-  pinMode(i,OUTPUT);
+  pinMode(i,OUTPUT);//defines digital pins from 8-13 as outputs
   }
 }
 
 void loop() {
-  for(int i=8;i<=13;i++)
+  for(int i=8;i<=13;i++) // forward
 {
  digitalWrite(i,HIGH);
  delay(50);
  digitalWrite(i,LOW);
  delay(50);
 }
- for(int i=13;i>=8;i--)
+ for(int i=13;i>=8;i--)// reverse
 {
  digitalWrite(i,HIGH);
  delay(50);
