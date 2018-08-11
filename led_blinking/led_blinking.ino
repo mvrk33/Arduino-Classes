9ba1313d1fa8e0383bb5fe069082be45ddef3329
Rajesh Krishna <38561827+mvrk33@users.noreply.github.com>
2018-08-11 07:32:11 +0530

    Day-1
    arduino basics
    led blinking

--- led_blinking/led_blinking.ino
+++ led_blinking/led_blinking.ino
@@ -1,10 +1,10 @@
 void setup() {
-  pinMode(12,OUTPUT);
+  pinMode(12,OUTPUT); // defines 12 pin as output
 }
 
 void loop() {
- digitalWrite(12,HIGH);
- delay(100);
+ digitalWrite(12,HIGH); // gives 5v to 12 pin 
+ delay(100); // waits for 100 milli sec
 digitalWrite(12,LOW);
  delay(100);
 }
